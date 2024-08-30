6ad70ea411801ee6229c0b6ab6a6c827a5ef842d
mohammed <mohammed.mahmoudfci@gmail.com>
2024-08-30 04:03:12 +0300

    edited

--- model_testing_script.py
+++ model_testing_script.py
@@ -1,9 +1,10 @@
 from tensorflow.python.keras.models import load_model
 
-model_path = 'model/autism_binary_handwritin_modelv.h5'
+model_path = 'E:/Documents/fci-zu/grad_project/flask_handwriting_API/model/autism_binary_handwritin_modelv.h5'
 
 try:
     model = load_model(model_path)
     print("Model loaded successfully")
 except Exception as e:
     print(f"Error loading model: {e}")
+
---
+++ test.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)  # This should print 2.14.0
+import keras
+print(keras.__version__)  # Should print 3.4.1 or the current Keras version
+
